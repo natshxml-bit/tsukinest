770aa15b44a8a790635fa7f6473dcd02f7d481ec
natshxml-bit <natshxml@gmail.com>
2026-07-11 20:22:55 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,9 +94,9 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      maxVideoPreview: -1,
-      maxImagePreview: "large",
-      maxSnippet: -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
 
